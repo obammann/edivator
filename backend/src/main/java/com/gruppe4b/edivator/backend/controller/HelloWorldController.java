65928855a27ab32d161ef52b17d4fc103b11bd61
jgreilich <jgreilich@stud.hs-bremen.de>
2017-05-30 11:21:04 +0200

    Added "Hello World"-Route in Backend

---
+++ backend/src/main/java/com/gruppe4b/edivator/backend/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.gruppe4b.edivator.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @RequestMapping("/helloworld")
+    public String helloworld() {
+        return "Hello World.";
+    }
+
+
+
+}
