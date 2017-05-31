91f8c1283026c9b8db7893d6efac2f7273742b0d
jgreilich <jgreilich@stud.hs-bremen.de>
2017-05-31 19:52:12 +0200

    Started implementing upload of images in frontend

--- src/main/webapp/js/EdivatorAngular.js
+++ src/main/webapp/js/EdivatorAngular.js
@@ -21,6 +21,43 @@ EdivatorModul.factory('Picture', function() {
     };
 });
 
-EdivatorModul.controller('PictureCtrl', function($scope, Picture){
+EdivatorModul.controller('PictureCtrl', function($scope, Picture, $http){
     $scope.picture = Picture;
+
+    $scope.uploadState = "No Image loaded.";
+    this.uploadPicture = function () {
+
+    	// https://stackoverflow.com/questions/18571001/file-upload-using-angularjs
+    	$scope.uploadState = "Loading...";
+
+        var f = document.getElementById('file').files[0],
+            r = new FileReader();
+
+        r.onloadend = function(e) {
+            var data = e.target.result;
+
+
+			//send your binary data via $http or $resource or do anything else with it
+			// https://stackoverflow.com/questions/25152700/angularjs-put-binary-data-from-arraybuffer-to-the-server
+			// Url fehlr
+			$http({
+				method: 'PUT',
+				headers: {'Content-Type': 'undefined'},
+				data: new Uint8Array(r.result),
+				transformRequest: []
+			});
+
+
+			// Other Solution:
+			// https://stackoverflow.com/questions/13963022/angularjs-how-to-implement-a-simple-file-upload-with-multipart-form
+
+            $scope.uploadState = "Uploaded Picture succesfully.";
+
+        }
+
+        r.readAsArrayBuffer(f);
+		// $scope.uploadState = "Image is uploaded.";
+    }
+
+
 });
