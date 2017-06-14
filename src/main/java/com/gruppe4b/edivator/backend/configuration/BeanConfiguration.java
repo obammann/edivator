80d3556a7402ac79fce062e7bc315ec21f23c8ed
obammann <o.bammann@ewetel.net>
2017-06-14 14:26:45 +0200

    Added classes from git repo for file upload; git repo: https://github.com/kernel164/gmultipart

--- src/main/java/com/gruppe4b/edivator/backend/configuration/BeanConfiguration.java
+++ src/main/java/com/gruppe4b/edivator/backend/configuration/BeanConfiguration.java
@@ -1,16 +1,17 @@
 package com.gruppe4b.edivator.backend.configuration;
 
+import com.gruppe4b.edivator.backend.fileUpload.GMultipartResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 public class BeanConfiguration {
 
     @Bean
     public MultipartResolver multipartResolver(){
-        return new CommonsMultipartResolver();
+//        return new CommonsMultipartResolver();
+        return new GMultipartResolver();
     }
 
 }
